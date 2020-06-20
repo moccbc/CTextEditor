6a03e8995df1b1c50dc9e67fd5d7662e05f2b1a8
moccbc <yutanak2010@gmail.com>
2020-06-20 09:51:21 -0700

    Cursor moves

--- kilo.c
+++ kilo.c
@@ -154,6 +154,23 @@ void abFree(struct abuf *ab) {
 }
 
 /*---------- Input Functions ----------*/
+void editorMoveCursor(char key) {
+    switch (key) {
+        case 'a':
+            E.cx--;
+            break;
+        case 'd':
+            E.cx++;
+            break;
+        case 'w':
+            E.cy--;
+            break;
+        case 's':
+            E.cy++;
+            break;
+    }
+}
+
 // This function waits for a keypress and then handles it.
 void editorProcessKeypress() {
     char c = editorReadKey();
@@ -163,6 +180,13 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+            break;
+
+        case 'w':
+        case 's':
+        case 'a':
+        case 'd':
+            editorMoveCursor(c);
             break;
     }
 }
@@ -211,8 +235,13 @@ void editorRefreshScreen() {
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
-    abAppend(&ab, "\x1b[H", 3);
-    abAppend(&ab, "\x1b[?25l", 6);
+
+    // Positions the cursor
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
+    abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     // Freeing the memory used by abuf
