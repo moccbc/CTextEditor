04d5809b9665b8e9888ed877b41138d0a667e9ea
moccbc <yutanak2010@gmail.com>
2020-06-20 07:22:38 -0700

    Displaying ASCII values of keypress

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h> // iscntrl()
+#include <stdio.h> // printf()
 #include <stdlib.h> // atexit()
 #include <termios.h> // struct termios, tcgetattr(), tcsetattr(), ECHO, TCSAFLUSH
 #include <unistd.h> // read() and STDIN_FILENO
@@ -13,7 +15,8 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    // Adding "| ICANON" makes the program read byte by byte instead of line by line
+    // Adding "| ICANON" makes the program read byte by byte instead of line by line.
+    // This disables the canonical mode ie get input by pressing enter mode.
     raw.c_lflag &= ~(ECHO | ICANON);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -26,6 +29,16 @@ int main() {
     // and will keep doing it until there aren't anymore bytes to read.
     // It returns the number of bytes that it read, and will return 0
     // when it reaches the end of the file.
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        // iscntrl() tests whether a char is a control character.
+        // ASCII codes 0 - 31, 127 are control characters, whereas
+        // ASCII codes 32 - 126 are all printable.
+        if (iscntrl(c)) {
+            printf("%d\n",c);
+        }
+        else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
     return 0;
 }
