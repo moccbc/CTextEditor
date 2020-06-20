c8e18ff648d81b1ae09d2dfc4b350e4b5b866284
moccbc <yutanak2010@gmail.com>
2020-06-20 08:56:57 -0700

    Fall back option for printing ~s when iotcl fails

--- kilo.c
+++ kilo.c
@@ -85,11 +85,36 @@ char editorReadKey() {
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
+
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+        // This is a fallback method for when ioctl doesn't work.
+        // c moves the cursor to the right and B moves the cursor down.
+        // We use 999 (a large number) to ensure that the cursor will move to the bottom
+        // right corner.
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
     }
     else {
         *cols = ws.ws_col;
@@ -97,6 +122,7 @@ int getWindowSize(int *rows, int *cols) {
         return 0;
     }
 }
+
 
 /*---------- Input Functions ----------*/
 // This function waits for a keypress and then handles it.
