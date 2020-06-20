21ee7510e3e03f886dd616d255fea29ab78b1a14
moccbc <yutanak2010@gmail.com>
2020-06-20 07:55:08 -0700

    Added error handling

--- kilo.c
+++ kilo.c
@@ -1,27 +1,43 @@
 #include <ctype.h> // iscntrl()
-#include <stdio.h> // printf()
+#include <errno.h> // errno, EAGAIN
+#include <stdio.h> // printf() perror()
 #include <stdlib.h> // atexit()
 #include <termios.h> // struct termios, tcgetattr(), tcsetattr(), ECHO, TCSAFLUSH, OPOST, IXON, ICANON, ISIG, IEXTEN
+// VMIN, VTIME
 #include <unistd.h> // read() and STDIN_FILENO
 
 struct termios orig_termios;
 
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+        die("tcsetattr");
+    }
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+        die("tcsetattr");
+    }
     atexit(disableRawMode);
+
+    // Adding BRKINT, INPCK, ISTRIP, and CS8 disables other miscellaneous
+    // flags, but they are there more for the legacy of enabling 
+    // "raw mode".
 
     struct termios raw = orig_termios;
     // This disables ctrl-s and ctrl-q.
     // These are inputs for software flow control.
     // Adding ICNRL fixes ctrl-m to be correctly read in bytes.
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     // This turns off output proessing. This is when each newline "\n"
     // is translated into "\r\n".
     raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     // Adding "| ICANON" makes the program read byte by byte instead of line by line.
     // This disables the canonical mode ie get input by pressing enter mode.
     // Adding ISIG local flag disables ctrl-c and ctrl-z
@@ -30,17 +46,29 @@ void enableRawMode() {
     // Adding IEXTEN disables ctrl-v.
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    //tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        die("tcsetattr");
+    }
 }
 
 int main() {
     enableRawMode();
-    char c;
+//    char c;
     // Asking read() to read 1 byte from the std input into the var c
     // and will keep doing it until there aren't anymore bytes to read.
     // It returns the number of bytes that it read, and will return 0
     // when it reaches the end of the file.
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+//    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while(1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+            die("read");
+        }
+        read(STDIN_FILENO, &c, 1);
         // iscntrl() tests whether a char is a control character.
         // ASCII codes 0 - 31, 127 are control characters, whereas
         // ASCII codes 32 - 126 are all printable.
@@ -50,6 +78,7 @@ int main() {
         else {
             printf("%d ('%c')\r\n", c, c);
         }
+        if (c == 'q') break;
     }
     return 0;
 }
